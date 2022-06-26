6e0b7ce58b7e65d5ad64417f4b510eb2ca312efd
Noé Zúñiga <brinknene@gmail.com>
2022-06-26 15:17:39 -0500

    jaja primer comit xd

---
+++ New Unity Project/Assets/Scripts/player_controller.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+	public float velocity = 10f;
+	public float time_speed = 0f;
+	
+	GameObject a;
+	
+
+	//time_speed = velocity * Time.deltaTime;
+	// Start is called before the first frame update
+	void Start(){}
+	// Update is called once per frame
+	void Update()
+	{
+		transform.rotation = new Vector3(0,0,0);
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += new Vector3(velocity, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += new Vector3(-velocity, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += new Vector3(0, 0, velocity);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position += new Vector3(0, 0, -velocity);
+		}
+
+	}
+}
